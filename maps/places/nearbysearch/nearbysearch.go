89890d7fe7e0d8b35eb2475a57c6271a055d938f
Simon Allen <simon@simonallen.org>
2014-12-27 23:07:43 +1100

    add docs

--- maps/places/nearbysearch/nearbysearch.go
+++ maps/places/nearbysearch/nearbysearch.go
@@ -1,3 +1,4 @@
+// Package nearbysearch implements Google Place's Nearby Search API.
 package nearbysearch
 
 import (
@@ -12,66 +13,82 @@ import (
 )
 
 const (
+	// APIURL is the URL to the API call.
 	APIURL = "https://maps.googleapis.com/maps/api/place/nearbysearch/json"
 )
 
+// PipeList represents a slice of strings to be separated by a pipe symbol.
 type PipeList []string
 
+// RequiredParams represents params required by the API.
 type RequiredParams struct {
 	APIKey   string          `url:"key"`
 	Location places.Location `url:"location"`
 }
 
+// OptionalRadiusParam optional param.
 type OptionalRadiusParam struct {
 	Radius int `url:"radius"`
 }
 
+// OptionalRankByParam optional param.
 type OptionalRankByParam struct {
 	RankBy string `url:"rankby"`
 }
 
+// OptionalTypesParam optional param.
 type OptionalTypesParam struct {
 	Types PipeList `url:"types"`
 }
 
+// OptionalKeywordParam optional param.
 type OptionalKeywordParam struct {
 	Keyword string `url:"keyword"`
 }
 
+// OptionalLanguageParam optional param.
 type OptionalLanguageParam struct {
 	Language string `url:"language"`
 }
 
+// OptionalMinPriceParam optional param.
 type OptionalMinPriceParam struct {
 	MinPrice int `url:"minprice"`
 }
 
+// OptionalMaxPriceParam optional param.
 type OptionalMaxPriceParam struct {
 	MaxPrice int `url:"maxprice"`
 }
 
+// OptionalNameParam optional param.
 type OptionalNameParam struct {
 	Name string `url:"name"`
 }
 
+// OptionalOpenNowParam optional param.
 type OptionalOpenNowParam struct {
 	OpenNow bool `url:"opennow"`
 }
 
+// OptionalPageTokenParam optional param.
 type OptionalPageTokenParam struct {
 	PageToken string `url:"pagetoken"`
 }
 
+// OptionalZagatSelectedParam optional param.
 type OptionalZagatSelectedParam struct {
 	ZagatSelected bool `url:"zagatselected"`
 }
 
+// EncodeValues encodes PipeList into into URL form.
 func (pipeList PipeList) EncodeValues(key string, v *url.Values) error {
 	v.Set(key, strings.Join([]string(pipeList), "|"))
 
 	return nil
 }
 
+// NearbySearch performs a search using Google's API.
 func NearbySearch(requiredParams *RequiredParams, optionalParams ...interface{}) (searchResponse *places.SearchResponse, err error) {
 	values, err := query.Values(requiredParams)
 
